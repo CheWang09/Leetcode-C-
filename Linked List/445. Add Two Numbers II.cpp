cf00bf37bd6c6c7aaa2cd38bd940cd26bae2e58c
Cheney Wang <32276346+CheWang09@users.noreply.github.com>
2017-10-24 23:15:02 -0400

    Update 445. Add Two Numbers II.cpp

--- Linked List/445. Add Two Numbers II.cpp
+++ Linked List/445. Add Two Numbers II.cpp
@@ -9,18 +9,18 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(!l1) return l1;
-        if(!l2) return l2;
+        if(l1->val==0) return l2;
+        if(l2->val==0) return l1;
         ListNode *l1head=l1;
         ListNode *l2head=l2;
         vector<int> nums1;
         vector<int> nums2;
-        while(l1head->next)
+        while(l1head)
         {
             nums1.push_back(l1head->val);
             l1head=l1head->next;
         }
-        while(l2head->next)
+        while(l2head)
         {
             nums2.push_back(l2head->val);
             l2head=l2head->next;
@@ -29,16 +29,17 @@ public:
         int j=nums2.size()-1;
         int k=i>j?i:j;
         int lengthK=k;
-        vector<int> result(k+1);
+        int mark=0;
+        vector<int> result;
         int addNum=0;int carry=0;int remainder=0;
-        while(i>=0||j>=0)
+        while(i>=0&&j>=0)
         {
             addNum=nums1[i]+nums2[j]+carry;
             if(addNum>=10)
             {
                 carry=1;
                 remainder=addNum-10;
-                result[k]=remainder;
+                result.push_back(remainder);
                 i--;
                 j--;
                 k--;
@@ -46,55 +47,64 @@ public:
             else
             {
                 carry=0;
-                result[k]=addNum;
+                result.push_back(addNum);
                 i--;
                 j--;
                 k--;
             }
             
         }
+        if(i==j&&carry==1)
+        {
+            result.push_back(carry);
+            mark=1;
+        }
         while(k>=0)
         {
-            if(i==0)
+            if(i<0)
             {
                 addNum=carry+nums2[j];
                 if(addNum>=10)
                 {
                     carry=1;
                     remainder=addNum-10;
-                    result[k]=remainder;
+                    result.push_back(remainder);
                     k--;
                     j--;
                 }
                 else
                 {
                     carry=0;
-                    result[k]=addNum;
+                    result.push_back(addNum);
                     k--;
                     j--;
                 }
                 
             }
-            if(j==0)
+            if(j<0&&i>=0)
             {
                 addNum=carry+nums1[i];
                 if(addNum>=10)
                 {
                     carry=1;
                     remainder=addNum-10;
-                    result[k]=remainder;
+                    result.push_back(remainder);
                     k--;
                     i--;
                 }
                 else
                 {
                     carry=0;
-                    result[k]=addNum;
+                    result.push_back(addNum);
                     k--;
                     i--;
                 }
                
             }
+        }
+        if(carry==1&&mark!=1)
+        {
+            result.push_back(carry);
         }
         ListNode *head;
         if(nums1.size()>nums2.size())
@@ -102,11 +112,24 @@ public:
         else
             head=l2;
         ListNode* resulthead=head;
-        for(int m=0;m<=lengthK;m++)
+        int m=result.size()-1;
+        while(head->next!=NULL)
         {
-              head->val=result[0];
-              head=head->next;
+              head->val=result[m];
+              head=head->next; 
+              m--;
         }
-        return resulthead;    
+        if(m==0)
+        {
+            head->val=result[m];
+            return resulthead;  
+        }
+        else
+        {
+            head->val=result[1];
+            head->next=new ListNode(result[0]);
+            return resulthead;  
+        }
+          
     }
 };
