f58192742a41ca60ec713a19b8870f7976bf125c
Cheney Wang <32276346+CheWang09@users.noreply.github.com>
2017-11-24 00:25:26 -0500

    Update 120.Triangle.cpp

--- Array/120.Triangle.cpp
+++ Array/120.Triangle.cpp
@@ -17,3 +17,27 @@ public:
         return MinP[0][0];
     }
 };
+
+
+
+//Algorithm optimization 
+
+//In the above algorithm, the space complexity is O(n^2), I think we are necessary to change our method by reducing the space complexity.
+// We can use a single vector to store the min value of each rows . 
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if(triangle.empty()) return 0;
+        //vector<vector<int>> MinP=triangle;
+        vector<int>sum=triangle[triangle.size()-1];
+        for(int i=triangle.size()-2;i>=0;i--)
+        {
+            for(int j=0;j<triangle[i].size();j++)
+            {
+                sum[j]=min(triangle[i][j]+sum[j],triangle[i][j]+sum[j+1]);
+            }
+        }
+        return sum[0];
+    }
+};
