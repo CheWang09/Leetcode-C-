7ed2a9d014b5a17006c7b9573d6eb2db7f42b1c6
Cheney Wang <32276346+CheWang09@users.noreply.github.com>
2017-12-14 23:42:29 -0500

    Update 15. 3Sum.cpp

--- Array/15. 3Sum.cpp
+++ Array/15. 3Sum.cpp
@@ -1,7 +1,58 @@
+class Solution {
+public: 
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size()<3) return res;
+        sort(nums.begin(),nums.end());
+        
+        vector<int> ans;
+        for(int i=0;i<nums.size()-2;++i)
+        {
+            if(i!=0&&nums[i]==nums[i-1])
+                continue;
+            ans.push_back(nums[i]);
+            helper(nums,i+1,nums[i],res,ans);
+            ans.pop_back();
+        }
+        return res;
+    }  
+    void helper(vector<int>& nums,int cur,int target,vector<vector<int>>& res,vector<int>& ans)
+    {
+        int left=cur;
+        int right=nums.size()-1;
+        
+        while(left<right)
+        {
+            int sum=nums[left]+nums[right];
+            
+            if(sum+target==0)
+            {
+                ans.push_back(nums[left]);
+                ans.push_back(nums[right]);
+                res.push_back(ans);
+                ans.pop_back();
+                ans.pop_back();
+                while(left<right&&nums[left]==nums[left+1])
+                    ++left;
+                while(left<right&&nums[right]==nums[right-1])
+                    --right;
+                ++left;
+                --right;
+            }
+            else if(sum+target<0)
+                ++left;
+            else
+                --right;
+        }
+        
+    }
+};
+
+
+
 /*
 ###
 ### some problem in this solution with the input[-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6] 
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -34,4 +85,33 @@ public:
         }
         return;
     }
-};*/
+};
+
+//recuresive solution that exceed time complexity/
+class Solution {
+public: 
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if(nums.empty()) return {};
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+        vector<int> ans;
+        helper(nums,0,3,0,res,ans);
+        return res;
+    }  
+    void helper(vector<int>& nums,int cur,int count,int target,vector<vector<int>>& res,vector<int>& ans)
+    {
+        if(count==0&&target==0)
+        {
+            res.push_back(ans);
+            return;
+        }
+        for(int i=cur;i<nums.size();i++)
+        {
+            if(i!=cur&&nums[i]==nums[i-1]) continue;
+            ans.push_back(nums[i]);
+            helper(nums,i+1,count-1,target-nums[i],res,ans);
+            ans.pop_back();
+        }
+    }
+};
+*/
