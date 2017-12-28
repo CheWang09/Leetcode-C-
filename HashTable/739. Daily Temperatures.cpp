a4ef343f1b80e186d9617bc1e1edb55285dbb8b5
Cheney Wang <32276346+CheWang09@users.noreply.github.com>
2017-12-27 22:53:45 -0500

    Update 739. Daily Temperatures.cpp

--- HashTable/739. Daily Temperatures.cpp
+++ HashTable/739. Daily Temperatures.cpp
@@ -1,41 +1,81 @@
-//This solution is time exceeded . And I have to optimize it .
+//Use array 
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        if(temperatures.empty()) return {};
+        vector<int> res(temperatures.size(),0); 
+        vector<pair<int,int>> VI;//value, index 
+        for(int i = 0; i < temperatures.size(); ++i){
+            while(!VI.empty()){
+                auto temp = VI[VI.size()-1];
+                if(temp.first < temperatures[i]){
+                    res[temp.second] = i - temp.second;
+                    VI.pop_back();
+                }else{
+                    break;
+                }
+            }
+            VI.push_back({temperatures[i], i});
+        }
+        return res;
+    }
+};
+
+
+//This solution is from first to last, and use stack
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        if(temperatures.empty()) return {};
+        vector<int> res(temperatures.size(),0); 
+        stack<pair<int,int>> sk;//value, index 
+        for(int i = 0; i < temperatures.size(); ++i){
+            while(!sk.empty()){
+                auto temp = sk.top();
+                if(temp.first < temperatures[i]){
+                    res[temp.second] = i - temp.second;
+                    sk.pop();
+                }else{
+                    break;
+                }
+            }
+            sk.push({temperatures[i], i});
+        }
+        return res;
+    }
+};
+
+
+//This solution is time exceeded ,not efficient. This solution is from last to first
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         if(temperatures.empty()) return {};
         
         vector<int> res(temperatures.size(),0);
-        
-        vector<int> temp;
         vector<int> index;
-        temp.push_back(temperatures[temperatures.size()-1]);
         index.push_back(temperatures.size()-1);
-        
-        res[temperatures.size()-1]=0;
-        
+        res[temperatures.size()-1]=0;     
         for(int i=temperatures.size()-2;i>=0;i--)
         {
-            int j = temp.size()-1;
+            int j = index.size()-1;
             while(j>=0)
             {
-                if(temperatures[i]>=temp[j])
+                if(temperatures[i]>=temperatures[index[j]])
                 {
-                    temp.pop_back();
                     index.pop_back();
-                    temp.push_back(temperatures[i]);
                     index.push_back(i);
                 }
-                if(temperatures[i]<temp[j])
+                if(temperatures[i]<temperatures[index[j]])
                 {
                     res[i]=index[j]-i;
-                    temp.push_back(temperatures[i]);
                     index.push_back(i);
                     break;
                 }
                 --j;
             }
         }
-        return res;
-        
+        return res;  
     }
 };
