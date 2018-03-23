bd5b2e5230377dc39129f6f9284a83b6972ad4d5
Cheney Wang <32276346+CheWang09@users.noreply.github.com>
2018-03-23 17:27:15 -0400

    Create 347. Top K Frequent Elements.py

---
+++ HashTable/347. Top K Frequent Elements.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        res = Counter(nums).most_common(k)
+        return [key for key, values in res]
